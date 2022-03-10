4eb63cd1e6b6193b4128dd00d08bdfe8e9ba9904
riscaldo <riccardo85ferrara@gmail.com>
2022-03-10 15:02:25 +0100

    check when user is connected

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,15 @@
 // Import Packages
 const express = require('express')
 const router = express.Router()
+    // const www = require('../bin/www')
 
-
-// routes
+//control
 router.get('/', (req, res) => {
     res.render('chat/chat')
 })
+
+// wwww.io.on('connection', (socket) => {
+//     console.log('a user connected');
+// });
 
 module.exports = router
