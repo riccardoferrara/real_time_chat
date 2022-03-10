5332e639484b67cee9e2f3a560ac9c0964836f22
riscaldo <riccardo85ferrara@gmail.com>
2022-03-10 19:17:39 +0100

    prevent duplicate msg from many sessions (done)

--- controllers/index.js
+++ controllers/index.js
@@ -7,8 +7,17 @@ const router = express.Router()
 router.get('/', (req, res) => {
 
     // use socket.io
+    let socket_id = [];
     var io = req.app.get('socketio')
+
     io.on('connection', (socket) => {
+        socket_id.push(socket.id);
+        if (socket_id[0] === socket.id) {
+            // remove the connection listener for any subsequent 
+            // connections with the same ID
+            io.removeAllListeners('connection');
+        }
+
         socket.on('chat message', (msg) => {
             io.emit('chat message', msg);
         });
