19d0ca63307ef948bb7fc8104731034bf9664d0b
riscaldo <riccardo85ferrara@gmail.com>
2022-03-10 19:02:02 +0100

    it works but the message is emitted twice, todo: exit io.on function from reouter.get request

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,23 @@
 // Import Packages
 const express = require('express')
 const router = express.Router()
-    // const www = require('../bin/www')
+
 
 //control
 router.get('/', (req, res) => {
+
+    // use socket.io
+    var io = req.app.get('socketio')
+    io.on('connection', (socket) => {
+        socket.on('chat message', (msg) => {
+            io.emit('chat message', msg);
+        });
+    });
+
+    // render page
     res.render('chat/chat')
+
 })
 
-// wwww.io.on('connection', (socket) => {
-//     console.log('a user connected');
-// });
 
 module.exports = router
