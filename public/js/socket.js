4dd1ae687292ebac6741797b2ffee6fbd505fc54
riscaldo <riccardo85ferrara@gmail.com>
2022-03-10 16:14:32 +0100

    broad cast from front to all connected users

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,5 +1,6 @@
 var socket = io();
 
+var messages = document.getElementById('messages');
 var form = document.getElementById('form');
 var input = document.getElementById('input');
 
@@ -9,4 +10,11 @@ form.addEventListener('submit', function(e) {
         socket.emit('chat message', input.value);
         input.value = '';
     }
+});
+
+socket.on('chat message', function(msg) {
+    var item = document.createElement('li');
+    item.textContent = msg;
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
 });
