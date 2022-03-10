3841117566319b85893e1c09b15ae3ad5f9cae01
riscaldo <riccardo85ferrara@gmail.com>
2022-03-10 13:23:19 +0100

    broadcast to everyone

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
+        console.log('message: ' + msg)
+        io.emit('chat message', msg)
     });
 });
 
