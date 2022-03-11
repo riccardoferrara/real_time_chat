714550b0013f384fad2437e433cbd52096afe660
riscaldo <riccardo85ferrara@gmail.com>
2022-03-11 10:51:50 +0100

    insert try/catch

--- controllers/messages.js
+++ controllers/messages.js
@@ -8,45 +8,49 @@ const Messages = require('../models/messages')
 
 //control
 router.get('/', async(req, res, next) => {
+    try {
+        // use socket.io
+        let socket_id = [];
+        var io = req.app.get('socketio')
 
-    // use socket.io
-    let socket_id = [];
-    var io = req.app.get('socketio')
+        //on user connection:
+        io.on('connection', async(socket) => {
 
-    //on user connection:
-    io.on('connection', async(socket) => {
+            //get all msgs from db
+            let db_msgs = await Messages.find().sort({ "create_date": 1 })
+            console.log(db_msgs)
+            db_msgs.forEach((msg, i) => {
+                console.log(`text_${i}:`, msg.txt)
+                io.emit('chat message', msg.text)
+            })
 
-        //get all msgs from db
-        let db_msgs = await Messages.find().sort({ "create_date": 1 })
-        console.log(db_msgs)
-        db_msgs.forEach((msg, i) => {
-            console.log(`text_${i}:`, msg.txt)
-            io.emit('chat message', msg.text)
-        })
+            //prevent duplicate msgs from many sessions
+            socket_id.push(socket.id);
+            if (socket_id[0] === socket.id) {
+                // remove the connection listener for any subsequent 
+                // connections with the same ID
+                io.removeAllListeners('connection');
+            }
 
-        //prevent duplicate msgs from many sessions
-        socket_id.push(socket.id);
-        if (socket_id[0] === socket.id) {
-            // remove the connection listener for any subsequent 
-            // connections with the same ID
-            io.removeAllListeners('connection');
-        }
+            //update front real-time msgs
+            socket.on('chat message', async(msg) => {
 
-        //update front real-time msgs
-        socket.on('chat message', async(msg) => {
+                // save msg to the db
+                console.log('msg: ', msg)
+                msg_db = await Messages.create({ 'text': msg })
+                if (msg_db) { console.log('msg created') }
 
-            // save msg to the db
-            console.log('msg: ', msg)
-            msg_db = await Messages.create({ 'text': msg })
-            if (msg_db) { console.log('msg created') }
-
-            // emit realt-time msg
-            io.emit('chat message', msg);
+                // emit realt-time msg
+                io.emit('chat message', msg);
+            });
         });
-    });
 
-    // render page
-    res.render('messages/list')
+        // render page
+        res.render('messages/list')
+
+    } catch {
+        next(err)
+    }
 
 })
 
